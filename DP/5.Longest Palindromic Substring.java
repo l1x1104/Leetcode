3d5d33f9153c9c4014109c582c8ad41bf6fa97e3
zmyvszk <zhoumingyang34@gmail.com>
2018-03-08 07:47:13 -0800

    new dp tests

---
+++ DP/5.Longest Palindromic Substring.java
@@ -0,0 +1,21 @@
+dp(i, j) represents whether s(i ... j) can form a palindromic substring, dp(i, j) is true when s(i) equals to s(j) and s(i+1 ... j-1) is a palindromic substring. 
+When we found a palindrome, check if it’s the longest one. Time complexity O(n^2).
+
+public String longestPalindrome(String s) {
+  int n = s.length();
+  String res = null;
+    
+  boolean[][] dp = new boolean[n][n];
+    
+  for (int i = n - 1; i >= 0; i--) {
+    for (int j = i; j < n; j++) {
+      dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+            
+      if (dp[i][j] && (res == null || j - i + 1 > res.length())) {
+        res = s.substring(i, j + 1);
+      }
+    }
+  }
+    
+  return res;
+}
